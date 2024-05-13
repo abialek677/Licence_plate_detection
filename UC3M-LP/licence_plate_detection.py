d89376dd2ecf5992a0a1948d686ec62fbe86f6eb
abialek677 <adam.bialek03@gmail.com>
2024-05-13 20:38:41 +0200

    not differentiating cars, adjusted saving data in csv file

--- UC3M-LP/licence_plate_detection.py
+++ UC3M-LP/licence_plate_detection.py
@@ -13,7 +13,7 @@ from util import get_car, read_license_plate, write_csv
 FRAME_HEIGHT = 456
 FRAME_WIDTH = 608
 
-results = {}
+results = []
 
 mot_tracker = Sort()
 
@@ -33,56 +33,45 @@ frame = cv2.imread(join(image_folder, image_file))
 frame_resized = cv2.resize(frame, (FRAME_WIDTH, FRAME_HEIGHT))  # Resize to 608x456
 original_h, original_w = frame.shape[:2]
 
-results[0] = {}
 
 # Detect vehicles
-detections = coco_model(frame_resized)[0]
-detections_ = []
-
-# Keep the original commented code intact
-# for detection in detections.boxes.data.tolist():
-#     x1, y1, x2, y2, score, class_id = detection
-#     if int(class_id) in vehicles:
-#         detections_.append([x1, y1, x2, y2, score])
-
-print("Detections: ", detections_)
-
-if not detections_:
-    detections_ = [[0, 0, frame_resized.shape[1], frame_resized.shape[0], 1.0]]
+detections = [[0, 0, frame_resized.shape[1], frame_resized.shape[0], 1.0]]
 
 # Check if there are detections before tracking
-if detections_:
     # Track vehicles
-    track_ids = mot_tracker.update(np.asarray(detections_))
-    print(track_ids)
-    # Detect license plates
-    license_plates = license_plate_detector(frame_resized)[0]
-    print("Licence plates: ", license_plates.boxes.data.tolist())
+track_ids = np.asarray(detections)
+print(track_ids)
+# Detect license plates
+license_plates = license_plate_detector(frame_resized)[0]
+print("Licence plates: ", license_plates.boxes.data.tolist())
 
-    for license_plate in license_plates.boxes.data.tolist():
-        x1, y1, x2, y2, score, class_id = license_plate
-        # Assign license plate to car
-        xcar1, ycar1, xcar2, ycar2, car_id = get_car(license_plate, track_ids)
-        print("Car id:", car_id)
-        if car_id != -1:
-            # Crop license plate
-            license_plate_crop = frame_resized[int(y1):int(y2), int(x1): int(x2), :]
 
-            # Process license plate
-            license_plate_crop_gray = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2GRAY)
-            _, license_plate_crop_thresh = cv2.threshold(license_plate_crop_gray, 64, 255, cv2.THRESH_BINARY_INV)
+for i, license_plate in enumerate(license_plates.boxes.data.tolist()):
+    x1, y1, x2, y2, score, class_id = license_plate
+    # Assign license plate to car
+    xcar1, ycar1, xcar2, ycar2, car_id = get_car(license_plate, detections)
+    print("Car id:", car_id)
+    if car_id != -1:
+        # Crop license plate
+        license_plate_crop = frame_resized[int(y1):int(y2), int(x1): int(x2), :]
 
-            # Read license plate number
-            license_plate_text, license_plate_text_score = read_license_plate(license_plate_crop_thresh)
-            print(license_plate_text)
-            if license_plate_text is not None:
-                results[0][car_id] = {'car': {'bbox': [xcar1, ycar1, xcar2, ycar2]},
-                                                  'license_plate': {'bbox': [x1*original_w/FRAME_WIDTH, y1*original_h/FRAME_HEIGHT, x2*original_w/FRAME_WIDTH, y2*original_h/FRAME_HEIGHT],
-                                                                    'text': license_plate_text,
-                                                                    'bbox_score': score,
-                                                                    'text_score': license_plate_text_score}}
-            # Draw rectangle around license plate
-            cv2.rectangle(frame, (int(x1*original_w/608), int(y1*original_h/456)), (int(x2*original_w/608), int(y2*original_h/456)), (0, 255, 0), 2)
+        # Process license plate
+        license_plate_crop_gray = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2GRAY)
+        _, license_plate_crop_thresh = cv2.threshold(license_plate_crop_gray, 64, 255, cv2.THRESH_BINARY_INV)
+
+        # Read license plate number
+        license_plate_text, license_plate_text_score = read_license_plate(license_plate_crop_thresh)
+        print(license_plate_text)
+
+        # Append results with necessary fields
+        results.append({'license_plate': {'bbox': [x1*original_w/FRAME_WIDTH, y1*original_h/FRAME_HEIGHT, x2*original_w/FRAME_WIDTH, y2*original_h/FRAME_HEIGHT],
+                                          'text': license_plate_text,
+                                          'bbox_score': score,
+                                          'text_score': license_plate_text_score}})
+
+        # Draw rectangle around license plate
+        cv2.rectangle(frame, (int(x1*original_w/FRAME_WIDTH), int(y1*original_h/FRAME_HEIGHT)), (int(x2*original_w/FRAME_WIDTH), int(y2*original_h/FRAME_HEIGHT)), (0, 255, 0), 2)
+
 
 # Convert BGR to RGB
 frame_rgb = cv2.cvtColor(frame_resized, cv2.COLOR_BGR2RGB)
@@ -92,5 +81,11 @@ plt.imshow(frame)
 plt.axis('off')  # Turn off axis labels
 plt.show()
 
+results_dict = {}
+for idx, result in enumerate(results):
+    results_dict[idx] = result
+
 # Write results
-write_csv(results, './test.csv')
+write_csv(results_dict, './test.csv')
+
+
--- UC3M-LP/util.py
+++ UC3M-LP/util.py
@@ -29,33 +29,25 @@ def write_csv(results, output_path):
         output_path (str): Path to the output CSV file.
     """
     with open(output_path, 'w') as f:
-        f.write('{},{},{},{},{},{},{}\n'.format('frame_nmr', 'car_id', 'car_bbox',
-                                                'license_plate_bbox', 'license_plate_bbox_score', 'license_number',
-                                                'license_number_score'))
+        f.write('{},{},{},{},{}\n'.format('frame_nmr', 'license_plate_bbox',
+                                       'license_plate_bbox_score', 'license_number', 'text_score'))
 
-        for frame_nmr in results.keys():
-            for car_id in results[frame_nmr].keys():
-                print(results[frame_nmr][car_id])
-                if 'car' in results[frame_nmr][car_id].keys() and \
-                   'license_plate' in results[frame_nmr][car_id].keys() and \
-                   'text' in results[frame_nmr][car_id]['license_plate'].keys():
-                    f.write('{},{},{},{},{},{},{}\n'.format(frame_nmr,
-                                                            car_id,
-                                                            '[{} {} {} {}]'.format(
-                                                                results[frame_nmr][car_id]['car']['bbox'][0],
-                                                                results[frame_nmr][car_id]['car']['bbox'][1],
-                                                                results[frame_nmr][car_id]['car']['bbox'][2],
-                                                                results[frame_nmr][car_id]['car']['bbox'][3]),
-                                                            '[{} {} {} {}]'.format(
-                                                                results[frame_nmr][car_id]['license_plate']['bbox'][0],
-                                                                results[frame_nmr][car_id]['license_plate']['bbox'][1],
-                                                                results[frame_nmr][car_id]['license_plate']['bbox'][2],
-                                                                results[frame_nmr][car_id]['license_plate']['bbox'][3]),
-                                                            results[frame_nmr][car_id]['license_plate']['bbox_score'],
-                                                            results[frame_nmr][car_id]['license_plate']['text'],
-                                                            results[frame_nmr][car_id]['license_plate']['text_score'])
-                            )
-        f.close()
+        for frame_number, result in results.items():
+            if 'license_plate' in result.keys() and 'text' in result['license_plate'].keys():
+                f.write('{},{},{},{},{}\n'.format(frame_number,
+                                                '[{} {} {} {}]'.format(
+                                                    result['license_plate']['bbox'][0],
+                                                    result['license_plate']['bbox'][1],
+                                                    result['license_plate']['bbox'][2],
+                                                    result['license_plate']['bbox'][3]),
+                                                result['license_plate']['bbox_score'],
+                                                result['license_plate']['text'],
+                                                result['license_plate']['text_score'])
+                        )
+
+
+
+
 
 
 def license_complies_format(text):
