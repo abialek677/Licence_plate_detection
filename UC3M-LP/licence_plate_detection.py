5bf4e72bd5937726cf6e2977fc7ec1ae5239d0e9
Magdalena Krampa <165083953+mgdkmp13@users.noreply.github.com>
2024-05-17 21:34:51 +0200

    Added YOLO text recognition
    Added YOLO text recognition

--- UC3M-LP/licence_plate_detection.py
+++ UC3M-LP/licence_plate_detection.py
@@ -2,7 +2,7 @@ from os.path import join
 import cv2
 from ultralytics import YOLO
 import matplotlib.pyplot as plt
-from util import read_license_plate, write_csv
+from util import read_licence_plate, write_csv
 
 # consts
 FRAME_HEIGHT = 456
@@ -12,10 +12,13 @@ FRAME_WIDTH = 608
 results = []
 
 # Load models
-license_plate_detector = YOLO('best100.pt')
+licence_plate_detector = YOLO('best100.pt')
+licence_plate_recognition = YOLO('best_letters.pt')
+#licence_plate_recognition = YOLO('best_letters_312.pt')
 
 # Folder containing images (adjust here)
-image_folder = "C:/Users/adamb/Desktop/Data/UC3M-LP-yolo/LP/images/val"
+#image_folder = "C:/Users/adamb/Desktop/Data/UC3M-LP-yolo/LP/images/val"
+image_folder = "C:/Users/Magda/Desktop/studia/sem4/sztuczna_inteligencja/UC3M-LP-yolo/LP/images/val"
 
 # input file
 image_file = "00000.jpg"
@@ -27,35 +30,45 @@ frame_resized = cv2.resize(frame, (FRAME_WIDTH, FRAME_HEIGHT))  # Resize to 608x
 # Specify search frame as whole image
 search_frame = [[0, 0, frame_resized.shape[1], frame_resized.shape[0], 1.0]]
 
-# Detect license plates
-license_plates = license_plate_detector(frame_resized)[0]
+# Detect licence plates
+licence_plates = licence_plate_detector(frame_resized)[0]
 
 
-for license_plate in license_plates.boxes.data.tolist():
-    x1, y1, x2, y2, score, _ = license_plate
-    # Crop license plate
-    license_plate_crop = frame_resized[int(y1):int(y2), int(x1): int(x2), :]
+for licence_plate in licence_plates.boxes.data.tolist():
+    licence_plate_text = ""
+    x1, y1, x2, y2, score, _ = licence_plate
+    # Crop licence plate
+    licence_plate_crop = frame_resized[int(y1):int(y2), int(x1): int(x2), :]
 
-    # Process license plate
-    license_plate_crop_gray = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2GRAY)
-    _, license_plate_crop_thresh = cv2.threshold(license_plate_crop_gray, 64, 255, cv2.THRESH_BINARY_INV)
+    # Process licence plate
+    licence_plate_crop_gray = cv2.cvtColor(licence_plate_crop, cv2.COLOR_BGR2GRAY)
 
-    # Read license plate number
-    license_plate_text, license_plate_text_score = read_license_plate(license_plate_crop_thresh)
-    print(license_plate_text)
+    # Read licence plate number
+    licence_plate_detections = licence_plate_recognition(licence_plate_crop)[0]
 
-    # Append results with necessary fields
-    results.append({'license_plate': {'bbox': [x1*original_w/FRAME_WIDTH, y1*original_h/FRAME_HEIGHT,
-                                               x2*original_w/FRAME_WIDTH, y2*original_h/FRAME_HEIGHT],
-                                      'text': license_plate_text,
-                                      'bbox_score': score,
-                                      'text_score': license_plate_text_score}})
+    # Sorting letter/numbers in order from left to right
+    sorted_detections = sorted(licence_plate_detections.boxes.data.tolist(), key=lambda x: x[0])
 
-    # Draw rectangle around license plate
+    for detection in sorted_detections:
+        x1_d, y1_d, x2_d, y2_d, score_d, class_id = detection
+        licence_plate_text += licence_plate_recognition.names[class_id]
+
+        # Append results with necessary fields
+        #results.append({'licence_plate': {'bbox': [x1*original_w/FRAME_WIDTH, y1*original_h/FRAME_HEIGHT,
+        #                                           x2*original_w/FRAME_WIDTH, y2*original_h/FRAME_HEIGHT],
+        #                                  'text': licence_plate_text,
+        #                                  'bbox_score': score}})
+
+    # Draw rectangle around licence plate
     cv2.rectangle(frame, (int(x1*original_w/FRAME_WIDTH), int(y1*original_h/FRAME_HEIGHT)),
                   (int(x2*original_w/FRAME_WIDTH), int(y2*original_h/FRAME_HEIGHT)), (0, 255, 0), 2)
 
-# Show the image with rectangles drawn around license plates
+    # Put a text above the licence plate
+    cv2.putText(frame, str(licence_plate_text),
+                (int(x1 * original_w / FRAME_WIDTH), int(y1 * original_h / FRAME_HEIGHT) - 10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+# Show the image with rectangles drawn around licence plates and licence plate number above
 plt.imshow(frame)
 plt.axis('off')
 plt.show()
