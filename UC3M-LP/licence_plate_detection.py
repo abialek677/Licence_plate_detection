901a1157cf3580c18918ac12bd02a21cdafe4546
Magdalena Krampa <165083953+mgdkmp13@users.noreply.github.com>
2024-05-18 20:50:29 +0200

    Added saving photos to a directory
    Added processing all files from a directory and saving results to another directory

--- UC3M-LP/licence_plate_detection.py
+++ UC3M-LP/licence_plate_detection.py
@@ -1,81 +1,69 @@
-from os.path import join
+from os import listdir, makedirs
+from os.path import join, exists
 import cv2
 from ultralytics import YOLO
 import matplotlib.pyplot as plt
 from util import read_licence_plate, write_csv
 
-# consts
+# Constants
 FRAME_HEIGHT = 456
 FRAME_WIDTH = 608
-
-
-results = []
+DETECTED_FOLDER = "detected"
+#DETECTED_FOLDER = "detected_val"
 
 # Load models
 licence_plate_detector = YOLO('best100.pt')
 licence_plate_recognition = YOLO('best_letters_312.pt')
 
-# Folder containing images (adjust here)
-#image_folder = "C:/Users/adamb/Desktop/Data/UC3M-LP-yolo/LP/images/val"
-image_folder = "C:/Users/Magda/Desktop/studia/sem4/sztuczna_inteligencja/UC3M-LP-yolo/LP/images/val"
+# Folder containing images
+image_folder = "C:/Users/Magda/Desktop/studia/sem4/sztuczna_inteligencja/UC3M-LP-yolo/LP/images/val/mycars"
+#image_folder = "C:/Users/Magda/Desktop/studia/sem4/sztuczna_inteligencja/UC3M-LP-yolo/LP/images/val"
 
-# input file
-image_file = "iwsonek2.png"
+# Create 'detected' subfolder if it doesn't exist
+if not exists(DETECTED_FOLDER):
+    makedirs(DETECTED_FOLDER)
 
-frame = cv2.imread(join(image_folder, image_file))
-original_h, original_w = frame.shape[:2]
-frame_resized = cv2.resize(frame, (FRAME_WIDTH, FRAME_HEIGHT))  # Resize to 608x456
+# Process each image in the folder
+for image_file in listdir(image_folder):
+    if image_file.endswith(('.jpg', '.jpeg', '.png')):
+        # Read the image
+        frame = cv2.imread(join(image_folder, image_file))
+        original_h, original_w = frame.shape[:2]
+        frame_resized = cv2.resize(frame, (FRAME_WIDTH, FRAME_HEIGHT))  # Resize to 608x456
 
-# Specify search frame as whole image
-search_frame = [[0, 0, frame_resized.shape[1], frame_resized.shape[0], 1.0]]
+        # Detect licence plates
+        licence_plates = licence_plate_detector(frame_resized)[0]
 
-# Detect licence plates
-licence_plates = licence_plate_detector(frame_resized)[0]
+        for licence_plate in licence_plates.boxes.data.tolist():
+            licence_plate_text = ""
+            x1, y1, x2, y2, score, _ = licence_plate
+            # Crop licence plate
+            licence_plate_crop = frame_resized[int(y1):int(y2), int(x1): int(x2), :]
 
+            # Process licence plate
+            licence_plate_crop_gray = cv2.cvtColor(licence_plate_crop, cv2.COLOR_BGR2GRAY)
 
-for licence_plate in licence_plates.boxes.data.tolist():
-    licence_plate_text = ""
-    x1, y1, x2, y2, score, _ = licence_plate
-    # Crop licence plate
-    licence_plate_crop = frame_resized[int(y1):int(y2), int(x1): int(x2), :]
+            # Read licence plate number
+            licence_plate_detections = licence_plate_recognition(licence_plate_crop)[0]
 
-    # Process licence plate
-    licence_plate_crop_gray = cv2.cvtColor(licence_plate_crop, cv2.COLOR_BGR2GRAY)
+            # Sorting letter/numbers in order from left to right
+            sorted_detections = sorted(licence_plate_detections.boxes.data.tolist(), key=lambda x: x[0])
 
-    # Read licence plate number
-    licence_plate_detections = licence_plate_recognition(licence_plate_crop)[0]
+            for detection in sorted_detections:
+                x1_d, y1_d, x2_d, y2_d, score_d, class_id = detection
+                if score_d > 0.5:
+                    licence_plate_text += licence_plate_recognition.names[class_id]
 
-    # Sorting letter/numbers in order from left to right
-    sorted_detections = sorted(licence_plate_detections.boxes.data.tolist(), key=lambda x: x[0])
+            # Draw rectangle around licence plate
+            cv2.rectangle(frame, (int(x1*original_w/FRAME_WIDTH), int(y1*original_h/FRAME_HEIGHT)),
+                          (int(x2*original_w/FRAME_WIDTH), int(y2*original_h/FRAME_HEIGHT)), (0, 255, 0), 2)
 
-    for detection in sorted_detections:
-        x1_d, y1_d, x2_d, y2_d, score_d, class_id = detection
-        if score_d > 0.7:
-            licence_plate_text += licence_plate_recognition.names[class_id]
+            # Put a text above the licence plate
+            cv2.putText(frame, str(licence_plate_text),
+                        (int(x1 * original_w / FRAME_WIDTH), int(y1 * original_h / FRAME_HEIGHT) - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
-        # Append results with necessary fields
-        #results.append({'licence_plate': {'bbox': [x1*original_w/FRAME_WIDTH, y1*original_h/FRAME_HEIGHT,
-        #                                           x2*original_w/FRAME_WIDTH, y2*original_h/FRAME_HEIGHT],
-        #                                  'text': licence_plate_text,
-        #                                  'bbox_score': score}})
+        # Save the processed image with detected licence plates
+        cv2.imwrite(join(DETECTED_FOLDER, image_file), frame)
 
-    # Draw rectangle around licence plate
-    cv2.rectangle(frame, (int(x1*original_w/FRAME_WIDTH), int(y1*original_h/FRAME_HEIGHT)),
-                  (int(x2*original_w/FRAME_WIDTH), int(y2*original_h/FRAME_HEIGHT)), (0, 255, 0), 2)
-
-    # Put a text above the licence plate
-    cv2.putText(frame, str(licence_plate_text),
-                (int(x1 * original_w / FRAME_WIDTH), int(y1 * original_h / FRAME_HEIGHT) - 10),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-
-# Show the image with rectangles drawn around licence plates and licence plate number above
-plt.imshow(frame)
-plt.axis('off')
-plt.show()
-
-results_dict = {}
-for idx, result in enumerate(results):
-    results_dict[idx] = result
-
-# Write results
-write_csv(results_dict, './test.csv')
+print("Processing complete.")
